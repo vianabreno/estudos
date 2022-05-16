df75e0a7c2d8c508d3532696f8945586bb2e5db1
vianabreno <brenoviana365@gmail.com>
2022-05-16 20:23:00 -0300

    Atualização 16.05.2022 16.05.2022

--- javascript/exercicios/exe009/exe002.js
+++ javascript/exercicios/exe009/exe002.js
@@ -43,12 +43,33 @@ while(estoque >=0){
     estoque--
 }*/
 
-var estoque = 0
+/*var estoque = 0
 
 while(estoque <=500){
     console.log(`Seu estoque agora é ${estoque}.`)
     estoque++
+}*/
+
+/*
+var senha = 1
+while(senha <= 10){
+    console.log(`Senha ${senha}`)
+    senha++
 }
+*/
 
+/*
+for(senha = 1; senha <= 20; senha++){
+    console.log(`Sua senha é ${senha}!`)
+}
+*/
 
+/*
+for(estoque = 0; estoque <= 100; estoque++){
+    console.log(`O estoque do biscoito é de ${estoque}!`)
+}
+*/
 
+for(estoque = 150; estoque >=0; estoque--){
+    console.log(`Meu estoque de biscoito é ${estoque}!!`)
+}
