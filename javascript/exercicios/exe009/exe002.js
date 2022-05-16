341d575d66279c609d05a80bc1c7b93bde6d3cfa
vianabreno <brenoviana365@gmail.com>
2022-05-16 19:54:09 -0300

    Atualizaçao

--- javascript/exercicios/exe009/exe002.js
+++ javascript/exercicios/exe009/exe002.js
@@ -15,4 +15,40 @@ while(senha <=100){
 
 }*/
 
-var
+/*var pizza = 8
+
+while(pizza >=0){
+    console.log(`Restam ${pizza} pedaços!`)
+    pizza--
+}*/
+
+/*var senha = 0
+
+while(senha <=20){
+    console.log(`Sua senha é ${senha}!`)
+    senha++
+}*/
+
+/*var pizza = 12
+
+while(pizza >=0){
+    console.log(`Restam ${pizza} pedaços!`)
+    pizza--
+}*/
+
+/*var estoque = 50
+
+while(estoque >=0){
+    console.log(`O estoque de biscoito é ${estoque}.`)
+    estoque--
+}*/
+
+var estoque = 0
+
+while(estoque <=500){
+    console.log(`Seu estoque agora é ${estoque}.`)
+    estoque++
+}
+
+
+
