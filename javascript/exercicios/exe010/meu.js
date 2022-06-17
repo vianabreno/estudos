db9d6d617cc49990da9ed35594780831b3dd5432
vianabreno <brenoviana365@gmail.com>
2022-06-17 08:34:01 -0300

    .
    .

---
+++ javascript/exercicios/exe010/meu.js
@@ -0,0 +1,4 @@
+function entrar(){
+    var teste = window.document.getElementById('teste')
+    teste.style.backgroundColor = 'green'
+}
---
+++ javascript/exercicios/exe010/meus.js
@@ -0,0 +1,5 @@
+var a = win document.getElementById('teste')
+a.addEventListener('click', clicar)
+function clicar(){
+    console.log('deubom!')
+}
